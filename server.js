9490b56bb08d0e9413322ac9493b8d04dd8706a9
Oleh <kravetsoleh4991@gmail.com>
2023-06-13 20:02:17 -0700

    Commit6

--- server.js
+++ server.js
@@ -6,10 +6,9 @@ const drinks = require('./models/drinks');
 app.set('view engine', 'ejs');
 
 
-app.get('/drinks', (req, res) => {
-  res.render('index', { drinks: drinks });
-});
-
+app.get('/drinks/:id', (req, res) => {
+    res.send(req.params.id);
+  });
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
