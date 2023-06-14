e5944ac34450d5825a9c7aa38fe1c4b1b2ad5358
Oleh <kravetsoleh4991@gmail.com>
2023-06-13 19:43:03 -0700

    Commit2

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express")
+
+const app = express()
+
+app.get("/", (req, res) => {
+    res.send("Welcome to the Gitpub App!")
+})
+
+
+app.listen(3000, () => {
+    console.log(`LISTENING ON PORT 3000`)
+})
