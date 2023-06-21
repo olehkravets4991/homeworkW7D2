6e49dc4f7562f4125ff84056a24043c3330307e6
Oleh <kravetsoleh4991@gmail.com>
2023-06-21 10:36:36 -0700

    Alcohol drinks linked

--- server.js
+++ server.js
@@ -1,87 +1,73 @@
-// const express = require('express');
-// const app = express();
-// const drinks = require('./models/drinks');
-
-
-// app.set('view engine', 'ejs');
-
-// app.use(express.static(__dirname + '/public'));
-
-
-// app.get('/drinks/:id', (req, res) => {
-//     const drinkId = req.params.id;
-//     const drink = drinks[drinkId];
-//     res.render('show', { drink: drink });
-//   });
-
-//   app.get('/drinks',(req, res) => {
-//     res.render('index')
-//   });
-
-// app.listen(3000, () => {
-//   console.log('Server is running on port 3000');
-// });
-
-// const express = require('express');
-// const app = express();
-// const port = 3000;
-
-// app.set('view engine', 'ejs');
-// app.use(express.static('public'));
-
-// app.get('/', (req, res) => {
-//   res.send('Welcome to the Gitpub App!');
-// });
-
-// app.get('/drinks', (req, res) => {
-//   const drinkNames = ['latte', 'cappuccino', 'espresso', 'macchiato'];
-//   res.render('index', { drinks: drinkNames });
-// });
-
-// app.get('/drinks/:id', (req, res) => {
-//   const drinkNames = ['latte', 'cappuccino', 'espresso', 'macchiato'];
-//   const drinkIndex = req.params.id;
-//   const drink = drinkNames[drinkIndex];
-//   res.render('show', { drink: drink });
-// });
-
-// app.listen(port, () => {
-//   console.log(`Server is running on port ${port}`);
-// });
 
 const express = require('express');
 const app = express();
 const port = 3000;
 
+// Array of drinks
+const drinks = [
+  {
+    name: 'cruddy mary',
+    price: 132,
+    image: 'https://imgur.com/Va5iIw5'
+  },
+  {
+    name: 'index on the beach',
+    price: 68,
+    image: 'https://imgur.com/XV2aPa2'
+  },
+  {
+    name: 'hack & coke',
+    price: 1,
+    image: 'https://imgur.com/rLOXFRI'
+  },
+  {
+    name: 'whiskey-value pair',
+    price: 11,
+    image: 'https://imgur.com/a7rmkoS'
+  },
+  {
+    name: '404 horsemen',
+    price: 202,
+    image: 'https://imgur.com/FLucOLr'
+  },
+  {
+    name: 'if stateMint julep',
+    price: 2,
+    image: 'https://imgur.com/yaoK0Dg'
+  },
+  {
+    name: 'APIPA',
+    price: 43,
+    image: 'https://imgur.com/qAhA7pi'
+  },
+  {
+    name: 'node to joy',
+    price: 56,
+    image: 'https://imgur.com/MbVdwZz'
+  },
+  // Additional Drinks
+  {
+    name: 'sour grapefruit',
+    price: 89,
+    image: 'https://imgur.com/NLkMCE6'
+  },
+  {
+    name: 'mint mojito',
+    price: 78,
+    image: 'https://imgur.com/YYq6d5t'
+  }
+];
+
+// Set the 'views' directory
+app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.send('Welcome to the Gitpub App!');
-});
-
+// Route to render the index.ejs template
 app.get('/drinks', (req, res) => {
-  const drinkNames = [
-    { name: 'latte', image: 'latte.png', price: '$3.99' },
-    { name: 'cappuccino', image: 'cappuccino.png', price: '$4.99' },
-    { name: 'espresso', image: 'espresso.png', price: '$2.99' },
-    { name: 'macchiato', image: 'macchiato.png', price: '$4.49' }
-  ];
-  res.render('index', { drinks: drinkNames });
+  res.render('index', { drinks: drinks });
 });
 
-app.get('/drinks/:id', (req, res) => {
-  const drinkNames = [
-    { name: 'latte', image: 'latte.png', price: '$3.99' },
-    { name: 'cappuccino', image: 'cappuccino.png', price: '$4.99' },
-    { name: 'espresso', image: 'espresso.png', price: '$2.99' },
-    { name: 'macchiato', image: 'macchiato.png', price: '$4.49' }
-  ];
-  const drinkIndex = req.params.id;
-  const drink = drinkNames[drinkIndex];
-  res.render('show', { drink: drink });
-});
-
+// Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
